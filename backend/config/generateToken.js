ab9b70d8ad143d2b51b4e6969b9190d58a20f749
alok singh <s.aloksingh7918@gmail.com>
2024-06-18 12:01:40 +0530

    change in connections

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,16 +1,6 @@
-// const jwt = require("jsonwebtoken");
-
-// const generateToken = (id) => {
-//   return jwt.sign({ id }, process.env.JWT_SECRET, {
-//     expiresIn: "30d",
-//   });
-// };
-
-// module.exports = generateToken;
 
 const jwt = require("jsonwebtoken");
 
-// Replace 'YOUR_SECRET_KEY' with your actual secret key
 const SECRET_KEY = 'asdfghjklrsdtfgdg';
 
 const generateToken = (id) => {
